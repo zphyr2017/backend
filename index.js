7d3a123fda5839306533e0d72d0a8e9cf3c45b1a
Matteo Turci <amministrazione@zephyraerospace.com>
2018-09-25 15:30:19 +0200

    a

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT;
 
+var obj = {'key':'value'};
+
+// Read Synchrously
+var fs = require("fs");
+
+var content = fs.readFileSync("data.json");
+console.log("Output Content : \n"+ content+"\n"+JSON.stringify(obj));
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/sapr/list.json', function (req, res) {
+  res.send(content);
+});
+
 
 /*Start the Server*/
 app.listen(PORT,function(){
