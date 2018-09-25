02ca0827641febc82f94531087c4124dde8736c8
Matteo Turci <amministrazione@zephyraerospace.com>
2018-09-25 15:07:02 +0200

    index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 /*Start the Server*/
-app.listen(3000,function(){
+app.listen(PORT,function(){
 console.log("It's Started on PORT 3000");
 });
